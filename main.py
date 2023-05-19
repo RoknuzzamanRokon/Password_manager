d8a86852165b4ffc1102435e8e9b229787031705
Rokon Ron <rokon.raz@gmail.com>
2023-05-19 06:54:32 +0600

    Create label and entry box.

--- main.py
+++ main.py
@@ -7,13 +7,43 @@ from tkinter import *
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
-window.config(padx=10,pady=10,bg='red')
+window.config(padx=5, pady=5, bg='red')
 
-
-canvas = Canvas(width=400,height=400)
+# Import image..
+canvas = Canvas(width=220,height=220)
 canvas_image = PhotoImage(file="logo.png")
-canvas.create_image(200,200,image=canvas_image)
+canvas.create_image(100, 100, image=canvas_image)
 canvas.pack()
+
+
+# Create Label..
+label_1 = Label(padx=5,pady=5,text="website:")
+label_1.pack()
+
+
+label_2 = Label(padx=5,pady=5,text="Email:")
+label_2.pack()
+
+
+label_3 = Label(padx=5,pady=5,text="PassWord:")
+label_3.pack()
+
+
+# Create Entry.
+entry_1 = Entry()
+entry_1.config(width=50,highlightthickness=2)
+entry_1.pack()
+
+
+entry_2 = Entry()
+entry_2.config(width=50,highlightthickness=2)
+entry_2.pack()
+
+
+entry_3 = Entry()
+entry_3.config(width=50,highlightthickness=2)
+entry_3.pack()
+
 
 
 window.mainloop()
