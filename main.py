c9b4e5d4973aa5517a7aee95efa806b798b06632
Rokon Ron <rokon.raz@gmail.com>
2023-05-19 16:22:24 +0600

    Create a function add_key.Here text file create and save the 'data.txt' file.

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ from tkinter import *
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def add_key():
+    website = entry_1.get()
+    email = entry_2.get()
+    password = entry_3.get()
+    with open("data.txt", mode='w') as data_file:
+        data_file.write(f"{website} | {email} | {password} ")
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -19,12 +27,8 @@ canvas.grid(row=1, column=2)
 # Create Label..
 label_1 = Label(padx=5, pady=5, text="Website:")
 label_1.grid(row=2, column=1)
-
-
 label_2 = Label(padx=5, pady=5, text="Email:")
 label_2.grid(row=3, column=1)
-
-
 label_3 = Label(padx=5, pady=5, text="PassWord:")
 label_3.grid(row=4, column=1)
 
@@ -32,13 +36,13 @@ label_3.grid(row=4, column=1)
 # Create Entry.
 entry_1 = Entry()
 entry_1.config(width=50, highlightthickness=0)
+entry_1.focus()
 entry_1.grid(row=2, column=2, columnspan=2)
-
 
 entry_2 = Entry()
 entry_2.config(width=50, highlightthickness=0)
+entry_2.insert(0, "rokon.ali@gmail.com")
 entry_2.grid(row=3, column=2, columnspan=2)
-
 
 entry_3 = Entry()
 entry_3.config(width=31, highlightthickness=0)
@@ -49,9 +53,9 @@ button_1 = Button()
 button_1.config(text="Generate Password")
 button_1.grid(row=4, column=3)
 
-
 button_2 = Button()
-button_2.config(text="Add", width=42)
+button_2.config(text="Add", width=42, command=add_key)
 button_2.grid(row=5, column=2, columnspan=2)
+
 
 window.mainloop()
