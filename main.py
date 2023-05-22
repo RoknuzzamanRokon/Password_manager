aaf3b8c659d7a731f0e2caa594b003146e3a1230
Rokon Ron <rokon.raz@gmail.com>
2023-05-22 06:31:00 +0600

    Error handling here.use try,except,else,finally.

--- main.py
+++ main.py
@@ -55,18 +55,24 @@ def add_key():
 
     # if message_box:
     else:
-        with open("data.json", mode='r') as data_file:
-            # Read the json file.
-            read_json = json.load(data_file)
+        try:
+            with open("data.json", mode='r') as data_file:
+                # Read the json file.
+                read_json = json.load(data_file)
+
+        except FileNotFoundError:
+            with open("data.json", mode="w") as data_file:
+                # write a json file.
+                json.dump(read_json, data_file, indent=4)
+
+        else:
             # Update a json file.
             read_json.update(new_date)
-            # write a json file.
-            json.dump(new_date, data_file, indent=4)
+            with open("data.json", mode="w") as data_file:
+                # write a json file.
+                json.dump(read_json, data_file, indent=4)
 
-
-
-
-            # data_file.write(f"{website} | {email} | {password}\n")
+        finally:
             entry_1.delete(0, END)
             entry_3.delete(0, END)
 
